230c59e47136a77652c8cf7db02bc8662ac7d37d
vpscl <vnspscl@proton.me>
2024-07-12 16:39:32 +0800

    Initial commit

---
+++ src/lib/components/ui/table/table.svelte
@@ -0,0 +1,24 @@
+<script>
+  import { cn } from "$lib/utils.js";
+  let className = undefined;
+  export { className as class };
+</script>
+
+<div class="relative w-full overflow-auto rounded-lg border">
+  <div class="p-4 flex justify-between border-b">
+    <h1 class="text-lg font-semibold">Users</h1>
+    <div class="">
+      <input
+        class="border rounded-sm px-2 py-1 text-sm"
+        placeholder="Search"
+        type="text"
+      />
+    </div>
+  </div>
+  <table
+    class={cn("w-full caption-bottom text-sm", className)}
+    {...$$restProps}
+  >
+    <slot />
+  </table>
+</div>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,16 @@
+<script>
+  import Table from "./table.svelte";
+  import { onMount } from "svelte";
+
+  let users = [];
+
+  onMount(async () => {
+    const response = await fetch("https://randomuser.me/api/?results=5");
+    const data = await response.json();
+    users = data.results;
+  });
+</script>
+
+<div class="container mx-auto py-10">
+  <Table {users} />
+</div>
